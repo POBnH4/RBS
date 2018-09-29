28bf8c5306f84a55a405f06099a5a9e92219a894
POBnH4 <bonchp4@yahoo.com>
2018-09-29 16:55:12 +0100

    asd

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const PASSWORD_VALIDITY = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,2
 
 // ----- - - - - - - - - - LOGIN --- - - - - - - -- - - - --  - --
 app.use(session({ secret: 'example'}));
-
 app.use(bodyParser.urlencoded({ extended: true}));
+app.set('view engine', 'ejs');
+
 
 var db;
 
